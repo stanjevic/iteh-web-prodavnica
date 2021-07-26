17a22aaf27016d871de07d28870721a370fca5fd
Stanka Tanjevic <stanka.tanjevic@gmail.com>
2021-07-27 00:14:24 +0200

    Kreiranje footer i header komonente

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { Container } from 'react-bootstrap'
 
+import Header from './components/Header'
+import Footer from './components/Footer'
 function App() {
   return (
     <div>
-     My app
+      <Header />
+     <main className="py-3">
+       <Container>
+       <h1>Welcome to our shop!</h1>
+       </Container>
+     </main>
+     <Footer />
     </div>
   );
 }
