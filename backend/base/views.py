7717d21056a0f7b26f0d6709d09a07f43c8db047
ssst553 <stojanka.tanjevic@gmail.com>
2021-07-27 21:22:22 +0200

    kreiranje baze

--- backend/base/views.py
+++ backend/base/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Product
 from .products import products
-
+from .serailizers import ProductSerializer
 # Create your views here.
 
 @api_view(['GET'])
@@ -28,14 +29,12 @@ def getRoutes (request):
 
 @api_view(['GET'])
 def getProducts(request):
-        return Response(products)
+        products = Product.objects.all()
+        serailizer = ProductSerializer(products,many=True)
+        return Response(serailizer.data)
 
 @api_view(['GET'])
 def getProduct(request,pk):
-    product=None
-
-    for i in products:
-        if i['_id'] == pk:
-            product = i
-            break
-    return Response(product)
+   product = Product.objects.get(_id=pk)
+   serailizer = ProductSerializer(product,many=False)
+   return Response(serailizer.data)
