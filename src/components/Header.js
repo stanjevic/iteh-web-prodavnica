a06e68332c152b134adccd7c86121dd76f24a94e
ssst553 <stojanka.tanjevic@gmail.com>
2021-07-27 12:30:51 +0200

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ function Header() {
     <Navbar.Brand >ProShop</Navbar.Brand>
     </LinkContainer>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
+    <Navbar.Collapse id="">
       <Nav className="mr-auto">
 
       <LinkContainer to='/cart'>
