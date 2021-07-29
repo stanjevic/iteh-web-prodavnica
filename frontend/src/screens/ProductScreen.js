f85bcdc3beaefa5a3602e7feb16e2462c73dbb10
Stanka Tanjevic <stanka.tanjevic@gmail.com>
2021-07-29 14:06:45 +0200

    Implementacija reduxa

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,26 +1,33 @@
 import React,{useState,useEffect} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { Row, Col,Image,ListGroup, Button, Card } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import axios from 'axios'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import { listProductDetails } from '../actions/productActions'
+
 
 
 function ProductScreen({match}) {
-   
-    const [product, setProduct] = useState([])
+    const dispatch = useDispatch()
+    const productDetails = useSelector(state=> state.productDetails)
+    const {loading, error, product}=productDetails
 
     useEffect(()=> {
-        async function fetchProduct(){
-            const{data} = await axios.get(`/api/products/${match.params.id}`)
-            setProduct(data)
-        }
-        fetchProduct()
-    },[])
+        dispatch(listProductDetails(match.params.id))
+    
+    },[dispatch, match])
 
     return (
         <div>
             <Link to='/' className="btn btn-light my-3">Go Back</Link>
-            <Row>
+            {loading ? 
+            <Loader />
+            : error
+            ? <Message variant='danger'> {error} </Message>
+            :(
+                <Row>
                 <Col md={6}>
                     <Image src={product.image} alt={product.name} fluid/>
                 </Col>
@@ -81,6 +88,11 @@ function ProductScreen({match}) {
                 </Col>
 
             </Row>
+            )
+            }
+
+
+           
         </div>
     )
 }
