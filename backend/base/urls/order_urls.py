cc568dca9ed02d954ec6b22a501c1a0f16c5dac8
Stojanka Tanjevic <stojanka.tanjevic@gmail.com>
2021-08-01 13:18:16 +0200

    Checkout proces

--- backend/base/urls/order_urls.py
+++ backend/base/urls/order_urls.py
@@ -5,5 +5,7 @@ from base.views import order_views as views
 
 
 urlpatterns = [
+    path('add/', views.addOrderItems, name='orders-add'),
+    
     
 ]
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -4,6 +4,7 @@ import { composeWithDevTools} from 'redux-devtools-extension'
 import {productListReducer, productDetailsReducer} from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
 import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer} from './reducers/userReducers'
+import { orderCreateReducer } from './reducers/orderReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -14,6 +15,8 @@ const reducer = combineReducers({
     userDetails: userDetailsReducer,
     userUpdateProfile: userUpdateProfileReducer,
 
+    orderCreate: orderCreateReducer,
+
 })
 
 const cartItemsFromStorage = localStorage.getItem('cartItems') ? 
@@ -22,8 +25,14 @@ const cartItemsFromStorage = localStorage.getItem('cartItems') ?
 const userInfoFromStorage = localStorage.getItem('userInfo') ? 
     JSON.parse(localStorage.getItem('userInfo')) : null
 
+const shippingAddressFromStorage = localStorage.getItem('shippingAddress') ? 
+JSON.parse(localStorage.getItem('shippingAddress')) : {}
+
 const initialState={
-    cart:{cartItems: cartItemsFromStorage},
+    cart:{
+    cartItems: cartItemsFromStorage,
+    shippingAddress:shippingAddressFromStorage
+    },
     userLogin:{ userInfo: userInfoFromStorage }
 }
 
