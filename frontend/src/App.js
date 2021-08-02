20cde24098cda78256c4c2fe1d25c26d2e805368
Stojanka Tanjevic <stojanka.tanjevic@gmail.com>
2021-08-02 16:27:03 +0200

    Podesavanje korisnika od strane admina

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,8 @@ import ShippingScreen from './screens/ShippingScreen'
 import PaymentScreen from './screens/PaymentScreen'
 import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import OrderScreen from './screens/OrderScreen'
+import UserListScreen from './screens/UserListScreen'
+import UserEditScreen from './screens/UserEditScreen'
 
 
 
@@ -32,6 +34,10 @@ function App() {
       <Route path='/payment' component={PaymentScreen} />
       <Route path='/placeorder' component={PlaceOrderScreen} />
       <Route path='/order/:id' component={OrderScreen} />
+
+      <Route path='/admin/userList' component={UserListScreen} />
+      <Route path='/admin/user/:id/edit' component={UserEditScreen} />
+
 
        </Container>
      </main>
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -47,6 +47,26 @@ function Header() {
           </LinkContainer>
         )}
 
+            {userInfo && userInfo.isAdmin && (
+                <NavDropdown title={'Admin'} id='adminmanue'>
+                   <LinkContainer to='/admin/userlist'>
+                       <NavDropdown.Item>Users</NavDropdown.Item>
+                    </LinkContainer>
+
+                    <LinkContainer to='/admin/productlist'>
+                       <NavDropdown.Item>Products</NavDropdown.Item>
+                    </LinkContainer>
+
+
+                    <LinkContainer to='/admin/orderlist'>
+                       <NavDropdown.Item>Orders</NavDropdown.Item>
+                    </LinkContainer>
+        
+
+                </NavDropdown>
+
+            )}
+
        
       </Nav>
     </Navbar.Collapse>
